89c9636637979eb9882acd76d547e0912897a123
Lakshitha-15 <lakshitha3513@email.com>
2026-03-26 07:59:11 +0000

    Add memory isolation using cgroups (OOM protection) with container restart support

--- runtime/container.c
+++ runtime/container.c
@@ -6,8 +6,13 @@
 #include <unistd.h>
 #include <signal.h>
 #include <time.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "container.h"
+
+void setup_memory_cgroup(Container *c);
+void add_to_cgroup(Container *c);
 
 #define STACK_SIZE (1024 * 1024)
 
@@ -39,14 +44,43 @@ int container_child(void *arg) {
     pid_t pid = getpid();
 
     while (1) {
+//          malloc(1024 * 1024); //1MB leak every loop
+
         printf("[Container %s] Running... PID=%d\n", c->id, pid);
-
         update_heartbeat(pid);
-
         sleep(1);
     }
 
     return 0;
+}
+
+// SET MEMORY LIMIT USING CGROUP
+void setup_memory_cgroup(Container *c) {
+    char path[512];
+
+    // create cgroup directory
+    snprintf(path, sizeof(path),
+             "/sys/fs/cgroup/%s", c->id);
+    mkdir(path, 0755);
+
+    // set memory limit
+    char mem_path[512];
+    snprintf(mem_path, sizeof(mem_path),
+             "%s/memory.max", path);
+
+    int fd = open(mem_path, O_WRONLY);
+    if (fd < 0) {
+        perror("open memory.max");
+        return;
+    }
+
+    char limit[64];
+    sprintf(limit, "%ld", c->mem_limit);
+
+    write(fd, limit, strlen(limit));
+    close(fd);
+
+    printf("[CGROUP] Memory limit set: %ld bytes\n", c->mem_limit);
 }
 
 int container_spawn(Container *c) {
@@ -68,10 +102,38 @@ int container_spawn(Container *c) {
     }
 
     c->pid = pid;
-
     printf("[RUNTIME] Container %s started with PID %d\n", c->id, pid);
-
+    //SET MEMORY LIMIT
+    setup_memory_cgroup(c);
+    //ADD PROCESS TO CGROUP
+    add_to_cgroup(c);
     register_container(c);
 
+/*
+    c->pid = pid;
+    printf("[RUNTIME] Container %s started with PID %d\n", c->id, pid);
+    register_container(c);
+*/
     return pid;
+}
+
+void add_to_cgroup(Container *c) {
+    char path[512];
+
+    snprintf(path, sizeof(path),
+             "/sys/fs/cgroup/%s/cgroup.procs", c->id);
+
+    int fd = open(path, O_WRONLY);
+    if (fd < 0) {
+        perror("open cgroup.procs");
+        return;
+    }
+
+    char pid_str[32];
+    sprintf(pid_str, "%d", c->pid);
+
+    write(fd, pid_str, strlen(pid_str));
+    close(fd);
+
+    printf("[CGROUP] Added PID %d to cgroup\n", c->pid);
 }
